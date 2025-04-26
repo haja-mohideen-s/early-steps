0355f39023411969606edbff730c99d5e4e1b140
haja-mohideen-s <99865664+haja-mohideen-s@users.noreply.github.com>
2025-04-26 09:50:00 +0800

    Add environment variables for CICD

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    locationUrl: 'https://data.gov.sg/api/action/datastore_search',
+    datasetId: 'resource_id',
+    datasetValue: 'd_696c994c50745b079b3684f0e90ffc53'
+};
